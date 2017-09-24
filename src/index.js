b29418adee89513624e5c340cdd63f4ff42a7dad
german <gpereira@travelit.com.ar>
2017-09-24 17:39:10 -0300

    se refactoriza el component app

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 
 const API_KEY ='AIzaSyCnE27qoKjpIu5JqaeeU1YBJrg9V_y2DRo';
 
-YTSearch({key:API_KEY,term:'pokemon'},function(data){
-  console.log(data);
-});
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state={  videos:[]  };
 
 
-const App =()=>{
-  return (
-      <div><SearchBar/></div>
-    
-  );
+    YTSearch({key:API_KEY,term:'pokemon'},(videos)=>{
+      this.setState( { videos } );
+    });
+
+
+  }
+
+
+  render(){
+     return (
+          <div><SearchBar/></div>      
+    );
+  }
 }
 
 ReactDOM.render(<App />,document.querySelector('.container'));
