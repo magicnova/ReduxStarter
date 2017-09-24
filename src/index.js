23850b198e4ae6f3a5949e7900c8d1d45f42d96a
german <gpereira@travelit.com.ar>
2017-09-24 17:23:22 -0300

    utilizando la api de youtube

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+
+const API_KEY ='AIzaSyCnE27qoKjpIu5JqaeeU1YBJrg9V_y2DRo';
+
+YTSearch({key:API_KEY,term:'pokemon'},function(data){
+  console.log(data);
+});
+
 
 const App =()=>{
-  return <div>Hi</div>;
+  return (
+      <div><SearchBar/></div>
+    
+  );
 }
 
 ReactDOM.render(<App />,document.querySelector('.container'));
